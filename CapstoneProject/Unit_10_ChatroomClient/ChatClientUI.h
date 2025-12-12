f078d8f07f8bb47008d0e35c514eb91a27bb18d4
Charly Nunez Polanco <110350236+Charlynpolanco@users.noreply.github.com>
2025-12-12 02:08:05 -0500

    Create ChatClientUI.h

---
+++ CapstoneProject/Unit_10_ChatroomClient/ChatClientUI.h
@@ -0,0 +1,28 @@
+#ifndef CHATCLIENTUI_H
+#define CHATCLIENTUI_H
+
+#include "MessageArea.h"
+#include "RoomList.h"
+#include "InputArea.h"
+
+class ChatClientUI {
+public:
+    ChatClientUI();
+    ~ChatClientUI();
+
+    void run();
+
+private:
+    void drawHeader();
+
+    MessageArea* messageArea;
+    RoomList* roomList;
+    InputArea* inputArea;
+
+    int h, w;
+    int topBarHeight;
+    int roomPanelWidth;
+    int messagePanelWidth;
+};
+
+#endif
