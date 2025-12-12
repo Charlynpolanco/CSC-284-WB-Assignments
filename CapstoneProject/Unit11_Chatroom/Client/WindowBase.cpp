0a0ca3bef5f8075f8599c8ff9890dbab3cb40371
Charly Nunez Polanco <110350236+Charlynpolanco@users.noreply.github.com>
2025-12-12 02:53:31 -0500

    Update WindowBase.cpp

--- CapstoneProject/Unit11_Chatroom/Client/WindowBase.cpp
+++ CapstoneProject/Unit11_Chatroom/Client/WindowBase.cpp
@@ -1,16 +1,17 @@
 #include "WindowBase.h"
 #include <stdexcept>
-
+//constructor
 WindowBase::WindowBase(int y, int x, int h, int w)
     : startY(y), startX(x), height(h), width(w) {
     win = newwin(height, width, startY, startX);
     if (!win) throw std::runtime_error("Failed to create window");
     keypad(win, TRUE);
 }
-
+//destructor
 WindowBase::~WindowBase() {
     if (win) {
         delwin(win);
         win = nullptr;
     }
+
 }
