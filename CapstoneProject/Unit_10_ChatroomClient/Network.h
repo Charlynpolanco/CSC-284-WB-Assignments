86d27848f8fddd5ba0a38aa3eb6fe17b70123f31
Charly Nunez Polanco <110350236+Charlynpolanco@users.noreply.github.com>
2025-12-12 01:52:35 -0500

    Delete CapstoneProject/Unit_10_ChatroomClient/Network.h

--- CapstoneProject/Unit_10_ChatroomClient/Network.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-
-int connectToServer(const std::string& ip, int port);
-int sendMessage(int sock, const std::string& msg);
-int receiveMessage(int sock, char* buffer, int bufferSize);
-void shutdownNetworking();
